4d3901f55efa49cb975cd12b8b21f13e26665638
akash-dubey <akash.dubey@salesforce.com>
2021-12-30 15:39:23 +0530

    adding print statements to see the stack

--- recursion_replace_char_in_string.py
+++ recursion_replace_char_in_string.py
@@ -1,11 +1,13 @@
 
 def replace_chars(s,a,b):
-    if len(s) == 0 :
+    if len(s) == 0 :        
         return s
     output=replace_chars(s[1:],a,b)
     if s[0] == a:
-        return str(b + output )
+        #print(b+output)
+        return b + output
     else:
+        #print(s[0]+output)
         return s[0]+ output
 
 print(replace_chars('my name is anthony', 'a', 'X'))
