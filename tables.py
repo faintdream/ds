8f8849d5c6235be0461821de60556442ba182ea9
akash-dubey <akash.dubey@salesforce.com>
2021-12-31 14:11:51 +0530

    update print statement

--- tables.py
+++ tables.py
@@ -6,18 +6,16 @@ def show_table(val, limit):
         return -1
     output = show_table(val, limit - 1)
     print(val, 'x', limit, '= ', val * limit)
-    return val * limit
+    return val * limit + output
 
 
 while True:
     try:
-        val = int(input(' Print table for  ? '))
+        val = int(input('Print table for  ? '))
         show_table(val, 12)
         next = input('press any key to continue or Q to quit ...')
         if next == 'q' or next == 'Q' :
             break
     except ValueError as e:
         print(e)
-        
-
-
+    
