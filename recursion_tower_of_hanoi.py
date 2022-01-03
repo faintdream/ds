9747a024452db0a7ecc0de8b1f6ef29e11863569
akash-dubey <akash.dubey@salesforce.com>
2022-01-04 00:48:08 +0530

    recursion towerof hanoi

---
+++ recursion_tower_of_hanoi.py
@@ -0,0 +1,20 @@
+#    ┌┐        ┌┐        ╔╗    
+#   ▓▓▓▓       ││        ║║    
+#  ▓▓▓▓▓▓      ││        ║║    
+# ▓▓▓▓▓▓▓▓     ││        ║║    
+#▓▓▓▓▓▓▓▓▓▓    ││        ║║    
+#────┴┴────────┴┴────────╨╨────
+#──SOURCE────HELPER────TARGET──
+
+
+def towerofhanoi(n, source, aux, dest): 
+    if n > 0:
+        towerofhanoi(n-1,source, dest, aux)
+        print(' move disk ',source, ' to ', dest )
+        towerofhanoi(n -1,aux,dest,source)
+        
+    else:
+        return 
+
+n=int(input())
+towerofhanoi(n, 'a','b','c')
